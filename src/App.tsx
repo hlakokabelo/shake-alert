c9147e3b9b072395baf0c2001d710f2017d6af3d
Kabelo Hlako <kabelohlako.kh@gmail.com>
2026-09-15 22:48:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
  return(
+<div>
+        <Toaster />
+
   <Routes>
             <Route path={'/'} element={<Home />} />
             <Route path="*" element={<NotFound />} />
-  </Routes>)
+  </Routes>
+</div>)
 }
 
 export default App;
